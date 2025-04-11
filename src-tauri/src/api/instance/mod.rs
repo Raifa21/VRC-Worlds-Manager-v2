dab91737f3aaf8852183d6e10d8956905b7575c2
Raifa21 <helloiamkotaro@gmail.com>
2025-04-11 22:41:32 +0700

    feat: expose create_instance logic in instance module

--- src-tauri/src/api/instance/mod.rs
+++ src-tauri/src/api/instance/mod.rs
@@ -6,3 +6,5 @@ pub use definitions::CreateInstanceRequestBuilder;
 pub use definitions::GroupOnlyInstanceConfig;
 pub use definitions::InstanceRegion;
 pub use definitions::InstanceType;
+
+pub use logic::create_instance;
