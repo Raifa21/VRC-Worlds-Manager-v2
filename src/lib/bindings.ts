9b121ebd712455c55bbb4a725c8c1af60ca87be0
Raifa21 <helloiamkotaro@gmail.com>
2025-06-20 13:50:43 +0700

    feat: add shareFolder command to invoke folder sharing functionality

--- src/lib/bindings.ts
+++ src/lib/bindings.ts
@@ -186,6 +186,17 @@ export const commands = {
       else return { status: 'error', error: e as any };
     }
   },
+  async shareFolder(folderName: string): Promise<Result<string, string>> {
+    try {
+      return {
+        status: 'ok',
+        data: await TAURI_INVOKE('share_folder', { folderName }),
+      };
+    } catch (e) {
+      if (e instanceof Error) throw e;
+      else return { status: 'error', error: e as any };
+    }
+  },
   async getTheme(): Promise<Result<string, string>> {
     try {
       return { status: 'ok', data: await TAURI_INVOKE('get_theme') };
