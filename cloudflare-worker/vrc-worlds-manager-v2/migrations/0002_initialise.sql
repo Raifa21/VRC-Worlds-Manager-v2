3dbbfed68fc3b0d3e01e91709c2371c207e31d7b
Raifa21 <helloiamkotaro@gmail.com>
2025-06-24 20:03:26 +0700

    chore/refactor: move external components to different git repository for better control. Moved to: https://github.com/Raifa21/vrc-worlds-manager-externs

--- cloudflare-worker/vrc-worlds-manager-v2/migrations/0002_initialise.sql
+++
@@ -1,9 +0,0 @@
--- Migration number: 0002 	 2025-06-20T08:01:23.353Z
-
-CREATE TABLE IF NOT EXISTS folders (
-  id           TEXT PRIMARY KEY,
-  hmac         TEXT NOT NULL,
-  name         TEXT NOT NULL,
-  expiration   TEXT NOT NULL,
-  created_at   TEXT NOT NULL
-);
