22e9b7cd6d4f41328df61945bcff9cce13b3482e
siloneco <siloneco4646@gmail.com>
2025-06-29 16:55:12 +0900

    refactor: separate memo rendering component

--- src/components/world-detail-popup.tsx
+++ src/components/world-detail-popup.tsx
@@ -34,6 +34,7 @@ import { GroupInstanceType, InstanceType, Region } from '@/types/instances';
 import { useLocalization } from '@/hooks/use-localization';
 import { Card, CardContent, CardHeader } from './ui/card';
 import { Textarea } from './ui/textarea';
+import MemoRenderer from './memo-renderer';
 
 export interface WorldDetailDialogProps {
   open: boolean;
@@ -704,61 +705,7 @@ export function WorldDetailPopup({
                     </div>
                     {!isEditingMemo && (
                       <div className="pr-4">
-                        <pre className="whitespace-pre-wrap text-base break-words font-sans h-full max-w-[700px]">
-                          {(memo ?? '').split('\n').map((line, index) => {
-                            let buffer: string[] = [];
-
-                            const URL_REGEX = /https?:\/\/[^\s]+/g;
-
-                            return (
-                              <p className="space-x-1" key={`${line}-${index}`}>
-                                {line.split(' ').map((text, index) => {
-                                  if (URL_REGEX.test(text)) {
-                                    const linkComponent = (
-                                      <a
-                                        key={`url-${text}-${index}`}
-                                        href={text}
-                                        className="text-blue-600 dark:text-blue-400 underline"
-                                        target="_blank"
-                                        rel="noreferrer noopener"
-                                      >
-                                        {text}
-                                      </a>
-                                    );
-
-                                    if (buffer.length <= 0) {
-                                      return linkComponent;
-                                    }
-
-                                    const bufferedText = buffer.join(' ');
-                                    buffer = [];
-
-                                    return (
-                                      <span
-                                        key={`text-url-${index}`}
-                                        className="space-x-1"
-                                      >
-                                        <span>{bufferedText}</span>
-                                        {linkComponent}
-                                      </span>
-                                    );
-                                  }
-
-                                  buffer = [...buffer, text];
-
-                                  return (
-                                    <Fragment key={`empty-${index}`}></Fragment>
-                                  );
-                                })}
-                                {buffer.length > 0 && (
-                                  <span key={`buffer-${line}-${index}`}>
-                                    {buffer.join(' ')}
-                                  </span>
-                                )}
-                              </p>
-                            );
-                          })}
-                        </pre>
+                        <MemoRenderer value={memo ?? ''} />
                       </div>
                     )}
                     {isEditingMemo && (
